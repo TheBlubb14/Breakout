ae8cd109f2df51cee94ef674301baf08fd583d00
Daniel <henk.daniel98@gmail.com>
2016-05-10 16:08:50 +0200

    neuen Game in Mono

--- Breakout/Breakout/Program.cs
+++ Breakout/Breakout/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Breakout
 {
-    static class Program
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
     {
         /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            using (var game = new Game1())
+                game.Run();
         }
     }
 }
